d2136535bcdad002fa6a777fc6c506bccaf59f53
Jordi Esteve <jesteve@zikzakmedia.com>
2014-08-26 19:59:32 +0200

    Add context to lot field in setup

--- move.py
+++ move.py
@@ -1,8 +1,7 @@
 #This file is part stock_lot_quantity module for Tryton.
-#The COPYRIGHT file at the top level of this repository contains 
+#The COPYRIGHT file at the top level of this repository contains
 #the full copyright notices and license terms.
 from trytond.pool import PoolMeta
-from trytond.model import fields
 from trytond.pyson import If, Eval, Bool
 
 __all__ = ['Move']
@@ -11,15 +10,15 @@ __metaclass__ = PoolMeta
 
 class Move:
     __name__ = 'stock.move'
-    lot = fields.Many2One('stock.lot', 'Lot',
-        domain=[
-            ('product', '=', Eval('product')),
-            ],
-        states={
-            'readonly': Eval('state').in_(['cancel', 'done']),
-            },
-        context={
-            'locations': If(Bool(Eval('warehouse', {})),
-                [Eval('warehouse', {})], []),
-            },
-        depends=['state', 'product'])
+
+    @classmethod
+    def __setup__(cls):
+        super(Move, cls).__setup__()
+
+        if not cls.lot.context:
+            cls.lot.context = {}
+        if not 'locations' in cls.lot.context:
+            cls.lot.context.update({
+                    'locations': If(Bool(Eval('warehouse', {})),
+                        [Eval('warehouse', {})], []),
+                    })
